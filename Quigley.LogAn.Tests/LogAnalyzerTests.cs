5b85c712673b0e333afa3cf1fc04afabfa414801
[MarcusQ] <marcusquigley@gmail.com>
2018-05-27 10:39:59 -0400

    Test for both cases

--- Quigley.LogAn.Tests/LogAnalyzerTests.cs
+++ Quigley.LogAn.Tests/LogAnalyzerTests.cs
@@ -6,14 +6,34 @@ namespace Quigley.LogAn.Tests
     [TestClass]
     public class LogAnalyzerTests
     {
+        private LogAnalyzer analyzer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            analyzer = new LogAnalyzer();
+        }
+
         [TestMethod]
         public void IsValidFileName_validFileLowerCased_ReturnsTrue()
         {
-            LogAnalyzer analyzer = new LogAnalyzer();
+            var result = analyzer.IsValidLogFileName("e.slf");
 
+            Assert.IsTrue(result, "filename not valid");
+        }
+
+        [TestMethod]
+        public void IsValidFileName_validFileUpperCased_ReturnsTrue()
+        {
             var result = analyzer.IsValidLogFileName("e.SLF");
 
             Assert.IsTrue(result, "filename not valid");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            analyzer = null;
         }
     }
 }
